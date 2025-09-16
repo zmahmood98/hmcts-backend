87c291f72f3b3561e6ca17edf2c99bfe68ec4882
zahra <zahra.mahmood98@hotmail.com>
2025-09-16 23:51:29 +0100

    Add Program.cs

---
+++ Program.cs
@@ -0,0 +1,22 @@
+
+using HmctsBackend.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+app.MapControllers();
+app.Run();
