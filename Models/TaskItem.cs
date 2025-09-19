2b68885f4344ea6bbaf80a6707fd2b8cc73511a7
zahra <zahra.mahmood98@hotmail.com>
2025-09-19 17:17:01 +0100

    Make Title a required field and make default DueDate 5 days from creation time

--- Models/TaskItem.cs
+++ Models/TaskItem.cs
@@ -3,9 +3,9 @@ namespace HmctsBackend.Models
     public class TaskItem
     {
         public int Id { get; set; }
-        public string Title { get; set; } = string.Empty;
+        public required string Title { get; set; }
         public string? Description { get; set; }
         public string Status { get; set; } = "To do";
-        public DateTime? DueDate { get; set; }
+        public DateTime DueDate { get; set; } = DateTime.Now.AddDays(5);
     }
 }
