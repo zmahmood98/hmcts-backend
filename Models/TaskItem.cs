b12ae4e07a1a08ca0b35a34418c804d26738d732
zahra <zahra.mahmood98@hotmail.com>
2025-09-19 16:58:55 +0100

    Change defualt value of Status for TaskItem object

--- Models/TaskItem.cs
+++ Models/TaskItem.cs
@@ -5,7 +5,7 @@ namespace HmctsBackend.Models
         public int Id { get; set; }
         public string Title { get; set; } = string.Empty;
         public string? Description { get; set; }
-        public string Status { get; set; } = "Pending";
+        public string Status { get; set; } = "To do";
         public DateTime? DueDate { get; set; }
     }
 }
