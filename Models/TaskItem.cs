7e6ed08571ba9a294e3a5880e050943a09489d57
zahra <zahra.mahmood98@hotmail.com>
2025-09-20 01:16:32 +0100

    Remove default dueDate

--- Models/TaskItem.cs
+++ Models/TaskItem.cs
@@ -6,6 +6,6 @@ namespace HmctsBackend.Models
         public required string Title { get; set; }
         public string? Description { get; set; }
         public string Status { get; set; } = "To do";
-        public DateTime DueDate { get; set; } = DateTime.Now.AddDays(5);
+        public DateTime DueDate { get; set; }
     }
 }
